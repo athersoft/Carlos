de45c5326db59bb6f028801f8c9f45ba12903452
Benjamin <benjamin.vega.m@mail.pucv.cl>
2024-05-25 18:13:45 -0400

    Arreglado Bug de que la cosa no cargaba nunca

--- ProyectoIonic1/src/app/login-page/login-page-routing.module.ts
+++ ProyectoIonic1/src/app/login-page/login-page-routing.module.ts
@@ -7,11 +7,13 @@ const routes: Routes = [
   {
     path: '',
     component: LoginPagePage
-  },
+  }
+  /*,
   {
     path: 'signup-page',
     loadChildren: () => import('../signup-page/signup-page.module').then( m => m.SignupPagePageModule)
   }
+  */
 ];
 
 @NgModule({
