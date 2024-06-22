aab83b92a8618236da43096dd1ff74bfbd582130
Benjamin <benjamin.vega.m@mail.pucv.cl>
2024-06-22 17:38:45 -0400

    ESTÁ VIVO, ESTÁ VIVO!!!!

--- Ionic/back-end/src/main.ts
+++ Ionic/back-end/src/main.ts
@@ -29,7 +29,7 @@ import mysql from 'mysql';
 
 //import dbops from './src/dbops';
 
-const PortMySql = 3306;
+const PORT = 5001;
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -40,14 +40,19 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+connection.query('SELECT * FROM user', function (error, results, fields) {
+  if (error) throw error;
+  console.log('Result: ', results[0].name);
+});
+
 const appMySql = express();
 
 appMySql.use(json());
 // ALSO ADD THE use routes
 //appMySql.use(dbops);
 
-appMySql.listen(PortMySql, function() {
-  console.log("Server running on localhost:"+PortMySql);
+appMySql.listen(PORT, function() {
+  console.log("Server running on localhost:"+PORT);
 })
 
 appMySql.get('/', function(req, res)
